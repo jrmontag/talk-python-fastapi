a7faf73c1b0f52f05a99abd55eb2563820869a06
Josh Montague <joshua.montague@gmail.com>
2022-11-11 16:44:23 -0700

    html and styles for ch5 page

--- ch05/main.py
+++ ch05/main.py
@@ -1,13 +1,17 @@
 import fastapi
 import uvicorn
-
+from starlette.requests import Request
+from starlette.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
 
 
 api = fastapi.FastAPI()
+templates = Jinja2Templates('templates')
+api.mount('/static', StaticFiles(directory='static'), name='static')
 
 @api.get('/')
-def index():
-    return "Hello wx app!"
+def index(request: Request):
+    return templates.TemplateResponse('home/index.html', {'request': request})
 
 if __name__ == '__main__':
     uvicorn.run(api, port=8000, host='127.0.0.1')
