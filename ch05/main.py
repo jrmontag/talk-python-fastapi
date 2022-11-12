d43415aed909e870afad7f33da384f861b743ef7
Josh Montague <joshua.montague@gmail.com>
2022-11-11 17:05:28 -0700

    reorg ch5 api paths

--- ch05/main.py
+++ ch05/main.py
@@ -1,17 +1,25 @@
 import fastapi
 import uvicorn
-from starlette.requests import Request
-from starlette.templating import Jinja2Templates
 from starlette.staticfiles import StaticFiles
 
+from api import weather_api
+from views import home
 
 api = fastapi.FastAPI()
-templates = Jinja2Templates('templates')
-api.mount('/static', StaticFiles(directory='static'), name='static')
 
-@api.get('/')
-def index(request: Request):
-    return templates.TemplateResponse('home/index.html', {'request': request})
+def configure():
+    configure_routing()
+
+def configure_routing():    
+    api.mount('/static', StaticFiles(directory='static'), name='static')
+    api.include_router(home.router)
+    api.include_router(weather_api.router)
+
 
 if __name__ == '__main__':
+    # devel
+    configure()
     uvicorn.run(api, port=8000, host='127.0.0.1')
+else:
+    # prod
+    configure()
---
+++ ch05/views/home.py
@@ -0,0 +1,14 @@
+import fastapi
+from starlette.templating import Jinja2Templates
+from starlette.requests import Request
+
+templates = Jinja2Templates('templates')
+router = fastapi.APIRouter()
+
+@router.get('/')
+def index(request: Request):
+    return templates.TemplateResponse('home/index.html', {'request': request})
+
+@router.get('/favico.ico')
+def favicon():
+    return fastapi.responses.RedirectResponse(url='/static/img/favicon.ico')
