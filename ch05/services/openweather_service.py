69e218bef221be74584822eb652d80f1622d4446
Josh Montague <joshua.montague@gmail.com>
2022-11-14 20:11:50 -0700

    ch05: async api by swapping requests for httpx

--- ch05/services/openweather_service.py
+++ ch05/services/openweather_service.py
@@ -1,21 +1,24 @@
 from typing import Optional
+import httpx
 
-import requests
 
 api_key: Optional[str] = None 
 
-def get_report(city: str, state: Optional[str], country: str, units: Optional[str] = 'metric'):
+async def get_report_async(city: str,
+                            state: Optional[str], 
+                            country: str, 
+                            units: Optional[str] = 'metric') -> dict:
     # API v2.5 endpoints are deprecated: https://openweathermap.org/current#name    
     if state:
         q = f"{city},{state},{country}"
     else:
         q = f"{city},{country}"
-    
+
     url = f"https://api.openweathermap.org/data/2.5/weather?q={q}&appid={api_key}&units={units}"
-    print(url)
-    
-    resp = requests.get(url)
-    resp.raise_for_status()
+
+    async with httpx.AsyncClient() as client:    
+        resp = await client.get(url)
+        resp.raise_for_status()
 
     data = resp.json()
     forecast = data['main']
